7e9afadb70763f6d45fe4bbaa6fcd00ac589b78e
Sanchos <alexandertretyakov>
2018-01-28 15:32:14 +0200

    MPV-finish

---
+++ js/main.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    document.querySelectorAll('.column--right img')[0].addEventListener('click', function () {
+        document.querySelector('.the-photo').style.backgroundColor = 'green'//'url("../images/house.png")'
+    });
+});
