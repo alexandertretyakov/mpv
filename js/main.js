64382218388f2f59d997605bac24e774378e6b4c
igorbr <Igor.Bronovytskyi@playtech.com>
2018-01-28 15:47:50 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
     document.querySelectorAll('.column--right img')[0].addEventListener('click', function () {
-        document.querySelector('.the-photo').style.backgroundColor = 'green'//'url("../images/house.png")'
+        document.querySelector('.pop-up').style.backgroundImage = 'url(images/house.png)';
     });
 });
