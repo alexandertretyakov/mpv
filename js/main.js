5e2eb23a2498f32bcd5143bd7c1793957a16c64f
igorbr <Igor.Bronovytskyi@playtech.com>
2018-02-04 14:47:57 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -2,32 +2,16 @@ window.addEventListener('load', function() {
     var $popUp = document.querySelector('.pop-up');
     var $overlay = document.querySelector('.overlay');
 
-    document.querySelectorAll('.column--right img')[0].addEventListener('click', function () {
-        $overlay.style.display = 'block';
-        $popUp.style.display = 'block';
-        $popUp.style.backgroundImage = 'url(https://oxvo.ru/wp-content/uploads/2016/08/The-Secret-Life-of-Pets-2-2.jpg)';
-    });
-
-    document.querySelectorAll('.column--right img')[1].addEventListener('click', function () {
-        $overlay.style.display = 'block';
-        $popUp.style.display = 'block';
-        $popUp.style.backgroundImage = 'url(https://pmcvariety.files.wordpress.com/2013/06/the-secret-life-of-pets.jpg?w=1000&h=562&crop=1)';
-    });
-
-    document.querySelectorAll('.column--right img')[2].addEventListener('click', function () {
-        $overlay.style.display = 'block';
-        $popUp.style.display = 'block';
-        $popUp.style.backgroundImage = 'url(http://zenitbol.ru/_nw/171/43331998.jpg)';
-    });
-
-    document.querySelectorAll('.column--right img')[3].addEventListener('click', function () {
-        $overlay.style.display = 'block';
-        $popUp.style.display = 'block';
-        $popUp.style.backgroundImage = 'url(http://cn1.nevsedoma.com.ua/images/2009/104/1/animalsG.jpg)';
+    document.querySelector('.column--right').addEventListener('click', function (e) {
+        if (e.target.tagName === 'IMG') {
+            $overlay.classList.add('open');
+            $popUp.classList.add('open');
+            $popUp.style.backgroundImage = 'url('+ e.target.dataset.bigImage +')';
+        }
     });
 
     $overlay.addEventListener('click', function () {
-        $overlay.style.display = 'none';
-        $popUp.style.display = 'none';
+        $overlay.classList.remove('open');
+        $popUp.classList.remove('open');
     });
 });
